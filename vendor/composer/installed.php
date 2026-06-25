9d48f0d2f377ad0d19febd47cb71bcd5d456b418
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2026-06-25 23:50:45 +0200

    feat(core): add Composer autoloading and dependency management files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alexandre/osteo-book',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '9229b8ffdf6a66e9a660f0a5b34bf4f49fc30f87',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alexandre/osteo-book' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '9229b8ffdf6a66e9a660f0a5b34bf4f49fc30f87',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
