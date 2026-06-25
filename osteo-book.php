ba8225b0b3e3268209edfa4c2b05f11d38279c89
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2026-06-25 21:48:43 +0200

    Add plugin metadata and requirements to osteo-book.php

--- osteo-book.php
+++ osteo-book.php
@@ -2,6 +2,17 @@
 
 /**
  * Plugin Name: OsteoBook
+ * Plugin URI: https://github.com/UnknownChick/Osteo-Book
+ * Description: A plugin to manage animal osteopathy appointments.
+ * Version: 0.1.0
+ * Requires at least: 6.0
+ * Requires PHP: 8.0
+ * Author: Alexandre Ferreira
+ * Author URI: https://alexandre-ferreira.fr
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: osteo-book
+ * Requires Plugins: secure-custom-fields
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
